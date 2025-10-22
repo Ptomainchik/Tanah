78562e8864bfda9df29f24845c031cb04b0c2bba
Ptomainchik <fantasticdenis@mail.ru>
2025-10-22 20:37:50 +0300

    StartDev1.1

---
+++ src/Components/HomePage/HomePage.tsx
@@ -0,0 +1,11 @@
+import classes from "../Styles/HomePage.module.css";
+
+export const HomePage = () => {
+    
+    return (
+        <div className={classes.homePage}>
+
+            
+        </div>
+    )
+}
