e76ee17a209447e0848bca597379fe065ac06cfd
Ptomainchik <fantasticdenis@mail.ru>
2025-10-25 04:47:49 +0300

    HomePage

--- src/Components/HomePage/HomePage.tsx
+++ src/Components/HomePage/HomePage.tsx
@@ -1,5 +1,7 @@
 import { useNavigate } from "react-router";
 import classes from "../../Styles/HomePage.module.css";
+import Logo from "../../Images/HomePageImages/LogoTanah.png";
+import Vine from "../../Images/HomePageImages/Vine.png";
 
 export const HomePage = () => {
     const navigate = useNavigate();
@@ -16,10 +18,43 @@ export const HomePage = () => {
     }
     return (
         <div className={classes.homePage}>
-            <h1>Танах</h1>
-            <button id="Races" className={classes.buttonRaces} onClick={() => {handleTransition("Races")}}>Расы</button>
-            <button id="Factions" className={classes.buttonRaces} onClick={() => {handleTransition("Factions")}}>Фракции</button>
-            <button id="Events" className={classes.buttonRaces} onClick={() => {handleTransition("Events")}}>Исторические события</button>
+            <div className={classes.overlayTopbar}>
+                <div className={classes.topbar}>
+                    <img className={classes.logo} src={Logo} alt="Logo" draggable="false"/>
+                </div>
+            </div>
+
+                <img className={classes.sideDecorationRight} src={Vine} alt="Vine" draggable="false"/>
+                <img className={classes.sideDecorationLeft} src={Vine} alt="Vine" draggable="false"/>
+
+            <div className={classes.overlayBlockButtonLeft}>
+                <div className={classes.backgroundBlockButton}>
+                    <button id="Races" className={classes.buttonRaces} onClick={() => {handleTransition("Races")}}>Расы</button>
+                    <button id="Factions" className={classes.buttonFactions} onClick={() => {handleTransition("Factions")}}>Фракции</button>
+                    <button id="Events" className={classes.buttonEvents} onClick={() => {handleTransition("Events")}}>События</button>
+                </div>
+            </div>
+
+            <div className={classes.overlayBlockText}>
+                <div className={classes.backgroundBlockText}>
+                    <h1>Мир Танаха</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque doloribus voluptates consequatur aliquam vero assumenda rem adipisci. 
+                        Distinctio, quaerat architecto omnis,
+                         obcaecati neque ratione blanditiis quo quia, fugit molestias sed!
+                    </p>
+                </div>
+            </div>
+
+            <div className={classes.overlayBlockButtonRight}>
+                <div className={classes.backgroundBlockButton}>
+                    <button id="Races" className={classes.buttonRaces} onClick={() => {handleTransition("Races")}}>Расы</button>
+                    <button id="Factions" className={classes.buttonFactions} onClick={() => {handleTransition("Factions")}}>Фракции</button>
+                    <button id="Events" className={classes.buttonEvents} onClick={() => {handleTransition("Events")}}>События</button>
+                </div>
+            </div>
+            
+                
         </div>
     )
 };
