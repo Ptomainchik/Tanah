8e61c40384ca5cf2337286d455157ac107cfa025
Ptomainchik <fantasticdenis@mail.ru>
2025-11-04 22:37:56 +0300

    DwarvenPages1.0

--- src/Components/Races/Dwarven/DwarvenRacesPage.tsx
+++ src/Components/Races/Dwarven/DwarvenRacesPage.tsx
@@ -2,8 +2,12 @@ import classes from "../../../Styles/Pages.module.css";
 import Mountian from "../../../Images/BearPeaksImages/DecorationBearPeaks.png";
 import Star from "../../../Images/HomePageImages/Star.png";
 import { useNavigate } from "react-router";
+import DwarvenRace1 from "../../../Images/BearPeaksImages/DwarvenRace.jpg";
+import DwarvenRace2 from "../../../Images/BearPeaksImages/DwarvenRaceWoman.jpg";
+import DwarvenRace3 from "../../../Images/BearPeaksImages/DwarvenRaceTwo.jpg";
+import DwarvenRace4 from "../../../Images/BearPeaksImages/DwarvenRaceWomanTwo.jpg";
 
-export const DwarvenRacesPage = (props: any) => {
+export const DwarvenRacesPage = () => {
 
     const navigate = useNavigate();
 
@@ -25,13 +29,51 @@ export const DwarvenRacesPage = (props: any) => {
                     <div className={classes.contentScrollRace}>
                         <h1>Гномы</h1>
                         <p>
+                            <img className={classes.imageRace1} src={DwarvenRace1} alt="DwarvenRace1" draggable="false"/>
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                             Debitis nemo velit ullam dolore 
                             repellendus cumque earum dolores quae odio et distinctio 
                             suscipit quaerat quo asperiores odit, architecto rem laudantium iusto!
-                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum, quasi perspiciatis doloremque itaque, dicta deserunt omnis ab impedit debitis architecto nisi necessitatibus natus maxime repellat temporibus tempora! Itaque, quia. Soluta!
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos mollitia modi suscipit tempora natus, rerum quis nam quisquam perspiciatis ducimus iusto eum consectetur, cupiditate repudiandae, inventore laboriosam perferendis sed doloribus?
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut ipsa quaerat nam fuga, totam, veniam illo odit eaque molestiae dicta omnis quidem, laudantium alias numquam doloribus ad recusandae quibusdam velit.
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum, quasi perspiciatis 
+                            doloremque itaque, dicta deserunt omnis ab impedit debitis architecto nisi necessitatibus 
+                            natus maxime repellat temporibus tempora! Itaque, quia. Soluta!
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos mollitia modi suscipit tempora natus, 
+                            rerum quis nam quisquam perspiciatis ducimus iusto eum consectetur, cupiditate repudiandae, 
+                            inventore laboriosam perferendis sed doloribus?
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut ipsa quaerat nam fuga, totam, 
+                            veniam illo odit eaque molestiae dicta omnis quidem, laudantium alias numquam doloribus ad recusandae quibusdam velit.
+                            <img className={classes.imageRace2} src={DwarvenRace2} alt="DwarvenRace2" draggable="false"/>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                            Debitis nemo velit ullam dolore 
+                            repellendus cumque earum dolores quae odio et distinctio 
+                            suscipit quaerat quo asperiores odit, architecto rem laudantium iusto!
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum, quasi perspiciatis 
+                            doloremque itaque, dicta deserunt omnis ab impedit debitis architecto nisi necessitatibus 
+                            natus maxime repellat temporibus tempora! Itaque, quia. Soluta!
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos mollitia modi suscipit tempora natus, 
+                            rerum quis nam quisquam perspiciatis ducimus iusto eum consectetur, cupiditate repudiandae, 
+                            inventore laboriosam perferendis sed doloribus?
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut ipsa quaerat nam fuga, totam, 
+                            veniam illo odit eaque molestiae dicta omnis quidem, laudantium alias numquam doloribus ad recusandae quibusdam velit.
+                            <img className={classes.imageRace3} src={DwarvenRace3} alt="DwarvenRace3" draggable="false"/>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                            Debitis nemo velit ullam dolore 
+                            repellendus cumque earum dolores quae odio et distinctio 
+                            suscipit quaerat quo asperiores odit, architecto rem laudantium iusto!
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum, quasi perspiciatis 
+                            doloremque itaque, dicta deserunt omnis ab impedit debitis architecto nisi necessitatibus 
+                            natus maxime repellat temporibus tempora! Itaque, quia. Soluta!
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos mollitia modi suscipit tempora natus, 
+                            rerum quis nam quisquam perspiciatis ducimus iusto eum consectetur, cupiditate repudiandae, 
+                            inventore laboriosam perferendis sed doloribus?
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut ipsa quaerat nam fuga, totam, 
+                            veniam illo odit eaque molestiae dicta omnis quidem, laudantium alias numquam doloribus ad recusandae quibusdam velit.
+                            <img className={classes.imageRace4} src={DwarvenRace4} alt="DwarvenRace4" draggable="false"/>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                            Debitis nemo velit ullam dolore 
+                            repellendus cumque earum dolores quae odio et distinctio 
+                            suscipit quaerat quo asperiores odit, architecto rem laudantium iusto!
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum, quasi perspiciatis 
                         </p>
                     </div>
                 </div>
