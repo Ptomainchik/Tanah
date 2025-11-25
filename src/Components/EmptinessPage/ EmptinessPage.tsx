e88699ceb25561727dfa34889e61127531a0a22d
Ptomainchik <fantasticdenis@mail.ru>
2025-11-25 05:33:44 +0300

    BlockEmptinessImages

--- src/Components/EmptinessPage/ EmptinessPage.tsx
+++ src/Components/EmptinessPage/ EmptinessPage.tsx
@@ -4,6 +4,9 @@ import Chain from "../../Images/EptinessImages/DecorationEmptiness.png";
 import Star from "../../Images/HomePageImages/Star.png";
 import EmptyEyes from "../../Images/Icons/EmptyEyes.png";
 import EmptinessImage1 from "../../Images/EptinessImages/BackgroundPageEmptiness.jpg";
+import EmptinessImage2 from "../../Images/EptinessImages/TheHypostasisOfHatred.jpg";
+import EmptinessImage3 from "../../Images/EptinessImages/TheHypostasisOfOblivion.jpg";
+import EmptinessImage4 from "../../Images/EptinessImages/TheHypostasisOfRebirth.jpg";
 
 export const EmptinessPage = () => {
     const navigate = useNavigate();
@@ -28,6 +31,30 @@ export const EmptinessPage = () => {
                     <p> 
                         <h1>Пустота</h1>
                         <img className={classes.imageHorizontally} src={EmptinessImage1} alt="EmptinessImage1" draggable="false"/>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Debitis nemo velit ullam dolore 
+                        repellendus cumque earum dolores quae odio et distinctio 
+                        suscipit quaerat quo asperiores odit, architecto rem laudantium iusto!
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum, quasi perspiciatis doloremque itaque, dicta deserunt omnis ab impedit debitis architecto nisi necessitatibus natus maxime repellat temporibus tempora! Itaque, quia. Soluta!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos mollitia modi suscipit tempora natus, rerum quis nam quisquam perspiciatis ducimus iusto eum consectetur, cupiditate repudiandae, inventore laboriosam perferendis sed doloribus?
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut ipsa quaerat nam fuga, totam, veniam illo odit eaque molestiae dicta omnis quidem, laudantium alias numquam doloribus ad recusandae quibusdam velit.
+                        <img className={classes.imageHorizontally} src={EmptinessImage2} alt="EmptinessImage2" draggable="false"/>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Debitis nemo velit ullam dolore 
+                        repellendus cumque earum dolores quae odio et distinctio 
+                        suscipit quaerat quo asperiores odit, architecto rem laudantium iusto!
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum, quasi perspiciatis doloremque itaque, dicta deserunt omnis ab impedit debitis architecto nisi necessitatibus natus maxime repellat temporibus tempora! Itaque, quia. Soluta!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos mollitia modi suscipit tempora natus, rerum quis nam quisquam perspiciatis ducimus iusto eum consectetur, cupiditate repudiandae, inventore laboriosam perferendis sed doloribus?
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut ipsa quaerat nam fuga, totam, veniam illo odit eaque molestiae dicta omnis quidem, laudantium alias numquam doloribus ad recusandae quibusdam velit.
+                        <img className={classes.imageHorizontally} src={EmptinessImage3} alt="EmptinessImage3" draggable="false"/>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Debitis nemo velit ullam dolore 
+                        repellendus cumque earum dolores quae odio et distinctio 
+                        suscipit quaerat quo asperiores odit, architecto rem laudantium iusto!
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum, quasi perspiciatis doloremque itaque, dicta deserunt omnis ab impedit debitis architecto nisi necessitatibus natus maxime repellat temporibus tempora! Itaque, quia. Soluta!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos mollitia modi suscipit tempora natus, rerum quis nam quisquam perspiciatis ducimus iusto eum consectetur, cupiditate repudiandae, inventore laboriosam perferendis sed doloribus?
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut ipsa quaerat nam fuga, totam, veniam illo odit eaque molestiae dicta omnis quidem, laudantium alias numquam doloribus ad recusandae quibusdam velit.
+                        <img className={classes.imageHorizontally} src={EmptinessImage4} alt="EmptinessImage4" draggable="false"/>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                         Debitis nemo velit ullam dolore 
                         repellendus cumque earum dolores quae odio et distinctio 
